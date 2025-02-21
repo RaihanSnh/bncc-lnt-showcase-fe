be247c94d00e45899e3db814780acfbd9587c8d5
RaihanSnh <natharaihans@gmail.com>
2025-02-22 00:26:59 +0700

    feat: add tan stack react router

---
+++ src/pages/UploadPage.tsx
@@ -0,0 +1,8 @@
+function UploadPage() {
+    return (
+        <h1>Upload Page</h1>
+
+    )
+}
+
+export default UploadPage
---
+++ src/router.tsx
@@ -0,0 +1,48 @@
+import { createRootRoute, createRoute, createRouter } from '@tanstack/react-router';
+
+import Root from './App';
+import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import UploadPage from './pages/UploadPage';
+
+const rootRoute = createRootRoute({
+  component: Root, 
+});
+
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/', 
+  component: LandingPage,
+});
+
+const loginRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: 'login', 
+  component: LoginPage,
+});
+
+const registerRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: 'register',
+  component: RegisterPage,
+});
+
+const uploadRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: 'upload', 
+  component: UploadPage,
+});
+
+const routeTree = rootRoute.addChildren([
+  indexRoute,
+  loginRoute,
+  registerRoute,
+  uploadRoute,
+]);
+
+const router = createRouter({
+  routeTree,
+});
+
+export default router;
